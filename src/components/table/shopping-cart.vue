8ff285e9f63ec4a34a7921a58e7a3cde6250d423
kim1996 <2390620198@qq.com>
2018-09-26 10:17:41 +0800

    newbr

---
+++ src/components/table/shopping-cart.vue
@@ -0,0 +1,47 @@
+<template>
+    <table class="table table-striped table-bordered table-hover dataTable no-footer">
+        <thead >
+            <tr>
+                <th>全选</th>
+                <th>物品</th>
+                <th>数量</th>
+                <th>单价</th>
+                <th>金额</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
