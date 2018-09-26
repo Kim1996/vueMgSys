dbda94fff9da429951137c5d621faefbefa8ee46
kim1996 <2390620198@qq.com>
2018-09-26 14:28:43 +0800

    newbr-temp

--- src/components/table/shopping-cart.vue
+++ src/components/table/shopping-cart.vue
@@ -35,7 +35,15 @@
                 <td>1</td>
                 <td>1</td>
             </tr> 
-              
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr> 
+
         </tbody>
     </table>
 </template>
